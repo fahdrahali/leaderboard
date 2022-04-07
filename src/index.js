e70eaa2a5f527e1e423d9c6e8f912680511357df
Fahd Rahali <rahali.fahd@gmail.com>
2022-04-07 16:26:31 +0100

    create a function render to display a list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import { getAllScoresService } from '../modules/scoreServices.js';
 import './style.css';
+
+const render = async () => {
+  const container = document.getElementById('score-list');
+  const scores = await getAllScoresService()
+    .then((result) => result);
+  scores.result.forEach((el) => {
+    container.innerHTML += `<li>${el.user}: ${el.score}</li>`;
+  });
+};
+
+render();
